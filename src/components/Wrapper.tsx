c4ab1ba9cfc6fccabc96819c292f4413ee23919f
yogaqf <yoga.qf@gmail>
2025-03-08 00:23:19 +0700

    restart: rebuild awal

--- src/components/Wrapper.tsx
+++
@@ -1,6 +0,0 @@
-const Wrapper = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex w-full justify-between md:flex-col">{children}</div>
-  );
-};
-export default Wrapper;
